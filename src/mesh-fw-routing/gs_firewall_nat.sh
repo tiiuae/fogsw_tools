82b1fb8eb1c33a7f6951ce4177f058bbd0b51f80
Sergei Petrov <sergei.petrov@unikie.com>
2022-08-03 15:25:35 +0300

    script converted to 1-line command

--- src/mesh-fw-routing/gs_firewall_nat.sh
+++ src/mesh-fw-routing/gs_firewall_nat.sh
@@ -1,3 +1,1 @@
-fog ssh mesh "iptables -P FORWARD ACCEPT"
-fog ssh mesh "iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE"
-fog ssh mesh "iptables -t nat -A POSTROUTING -s 192.168.32.0/24 -o eth0 -j MASQUERADE"
+fog ssh mesh "iptables -P FORWARD ACCEPT" && fog ssh mesh "iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE" && fog ssh mesh "iptables -t nat -A POSTROUTING -s 192.168.32.0/24 -o eth0 -j MASQUERADE"
